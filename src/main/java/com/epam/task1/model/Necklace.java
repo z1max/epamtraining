524b0cb177102b1ab523ba717b3ebeb17d99474c
z1max <forcazimaxqwe123>
2018-05-10 11:52:03 +0300

    Add name field, update equals & hashCode

--- src/main/java/com/epam/task1/model/Necklace.java
+++ src/main/java/com/epam/task1/model/Necklace.java
@@ -1,22 +1,24 @@
 package com.epam.task1.model;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class Necklace {
+    private String name;
+
     private List<Jewel> jewels;
 
-    public Necklace(){
+    public Necklace(String name){
+        this.name = name;
         this.jewels = new ArrayList<>();
     }
 
-    public Necklace(List<Jewel> jewels) {
+    public Necklace(String name, List<Jewel> jewels) {
+        this.name = name;
         this.jewels = jewels;
     }
 
-    public Necklace(Jewel... jewels){
+    public Necklace(String name, Jewel... jewels){
+        this.name = name;
         this.jewels = Arrays.asList(jewels);
     }
 
@@ -32,9 +34,17 @@ public class Necklace {
         jewels.add(jewel);
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
-        return this.getClass().getName() + "{jewels=" + jewels + '}';
+        return this.getClass().getName() + "{name=" + name + "jewels=" + jewels + '}';
     }
 
     @Override
@@ -42,12 +52,19 @@ public class Necklace {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Necklace necklace = (Necklace) o;
-        return Objects.equals(jewels, necklace.jewels);
+        return name.equals(necklace.name) &&
+                jewels.equals(necklace.jewels);
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(jewels);
+        int hash = 7;
+        hash = 31 * hash + (name == null ? 0 : name.hashCode());
+        Iterator<Jewel> iterator = jewels.iterator();
+        while (iterator.hasNext()){
+            Jewel next = iterator.next();
+            hash = 31 * hash + (next == null ? 0 : next.hashCode());
+        }
+        return hash;
     }
 }
