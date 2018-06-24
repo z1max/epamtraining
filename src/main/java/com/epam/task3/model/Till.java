7ef3886b70dcaf288ac87e396c3974fd40b0f626
z1max <forcazimaxqwe123>
2018-06-24 14:48:28 +0300

    Change semaphore to lock

--- src/main/java/com/epam/task3/model/Till.java
+++ src/main/java/com/epam/task3/model/Till.java
@@ -5,7 +5,6 @@ import org.apache.log4j.Logger;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Till {
@@ -18,7 +17,6 @@ public class Till {
     private static int counter = 0;
     private int id = ++counter;
 
-    private Semaphore semaphore = new Semaphore(1);
     private ReentrantLock lock = new ReentrantLock();
     private volatile List<Customer> line = new LinkedList<>();
 
@@ -32,16 +30,16 @@ public class Till {
 
     public void serve(Customer customer) {
         try {
-            semaphore.acquire();
+            lock.lock();
             LOG.info(this + " is serving " + customer);
             //Обработка заказа. Время на обработку между MIN и  MAX
             Thread.sleep(RandomUtil.getIntBetween(MIN_SERVICE_TIME, MAX_SERVICE_TIME));
-            semaphore.release();
             LOG.info(this + " ended serving " + customer);
         } catch (InterruptedException e) {
             LOG.error(e);
+        } finally {
+            lock.unlock();
         }
-
     }
 
     public void addToLine(Customer customer){
