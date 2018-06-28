de9a6c7762a97293083d4e903f6629a96c765f63
z1max <forcazimaxqwe123>
2018-06-28 13:01:21 +0300

    Refactoring

--- src/main/java/com/epam/task1/parser/StAXNecklaceParser.java
+++ src/main/java/com/epam/task1/parser/StAXNecklaceParser.java
@@ -15,67 +15,62 @@ import java.io.InputStream;
 public class StAXNecklaceParser {
     private static final Logger LOG = Logger.getLogger(StAXNecklaceParser.class);
 
-    public Necklace parse(String fileName){
+    public Necklace parse(String fileName) throws FileNotFoundException, XMLStreamException {
         Necklace necklace = new Necklace();
         Jewel jewel = new Jewel();
 
         XMLInputFactory factory = XMLInputFactory.newInstance();
-        try {
-            InputStream input = new FileInputStream(fileName);
-            XMLStreamReader reader = factory.createXMLStreamReader(input);
 
-            while (reader.hasNext()){
-                int type = reader.next();
-                switch (type){
-                    case XMLStreamConstants.START_DOCUMENT : {
-                        LOG.info("Start parsing file");
-                        break;
-                    }
-                    case XMLStreamConstants.END_DOCUMENT : {
-                        LOG.info("End parsing file");
-                        break;
-                    }
-                    case XMLStreamConstants.START_ELEMENT : {
-                        String elementName = reader.getLocalName();
-                        LOG.info("Start handle element " + elementName);
-                        switch (elementName){
-                            case "necklace" : {
-                                necklace = new Necklace(reader.getAttributeValue(null, "name"));
-                                break;
-                            }
-                            case "jewel" : {
-                                jewel = new Jewel(reader.getAttributeValue(null, "name"));
-                                break;
-                            }
-                            case "weight" : {
-                                jewel.setWeight(Double.valueOf(reader.getElementText()));
-                                break;
-                            }
-                            case "price" : {
-                                jewel.setPrice(Long.valueOf(reader.getElementText()));
-                                break;
-                            }
-                            case "refractive-index" : {
-                                jewel.setRefractiveIndex(Double.valueOf(reader.getElementText()));
-                                break;
-                            }
+        InputStream input = new FileInputStream(fileName);
+        XMLStreamReader reader = factory.createXMLStreamReader(input);
+
+        while (reader.hasNext()) {
+            int type = reader.next();
+            switch (type) {
+                case XMLStreamConstants.START_DOCUMENT: {
+                    LOG.info("Start parsing file");
+                    break;
+                }
+                case XMLStreamConstants.END_DOCUMENT: {
+                    LOG.info("End parsing file");
+                    break;
+                }
+                case XMLStreamConstants.START_ELEMENT: {
+                    String elementName = reader.getLocalName();
+                    LOG.info("Start handle element " + elementName);
+                    switch (elementName) {
+                        case "necklace": {
+                            necklace = new Necklace(reader.getAttributeValue(null, "name"));
+                            break;
                         }
-                        break;
-                    }
-                    case XMLStreamConstants.END_ELEMENT : {
-                        String elementName = reader.getLocalName();
-                        LOG.info("End handling element " + elementName);
-                        if (elementName.equals("jewel")){
-                            necklace.addJewel(jewel);
-                            jewel = null;
+                        case "jewel": {
+                            jewel = new Jewel(reader.getAttributeValue(null, "name"));
+                            break;
                         }
+                        case "weight": {
+                            jewel.setWeight(Double.valueOf(reader.getElementText()));
+                            break;
+                        }
+                        case "price": {
+                            jewel.setPrice(Long.valueOf(reader.getElementText()));
+                            break;
+                        }
+                        case "refractive-index": {
+                            jewel.setRefractiveIndex(Double.valueOf(reader.getElementText()));
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case XMLStreamConstants.END_ELEMENT: {
+                    String elementName = reader.getLocalName();
+                    LOG.info("End handling element " + elementName);
+                    if (elementName.equals("jewel")) {
+                        necklace.addJewel(jewel);
+                        jewel = null;
                     }
                 }
             }
-        } catch (FileNotFoundException e) {
-            LOG.error("File not found!", e);
-        } catch (XMLStreamException e) {
-            LOG.error("Error parsing file", e);
         }
         return necklace;
     }
--- src/main/java/com/epam/task1/runner/Main.java
+++ src/main/java/com/epam/task1/runner/Main.java
@@ -1,60 +1,104 @@
 package com.epam.task1.runner;
 
 import com.epam.task1.model.Necklace;
+import com.epam.task1.parser.DOMParser;
 import com.epam.task1.parser.SAXNecklaceParser;
 import com.epam.task1.parser.StAXNecklaceParser;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class);
+    private static final String XSD_PATH = "src/main/resources/task4/necklace.xsd";
+    private static final String XML_PATH = "src/main/resources/task4/necklace.xml";
 
     public static void main(String[] args) {
-        validateXml("src/main/resources/task4/necklace.xsd", "src/main/resources/task4/necklace.xml");
-
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-
         try {
-            javax.xml.parsers.SAXParser parser = factory.newSAXParser();
-            SAXNecklaceParser necklaceParser = new SAXNecklaceParser();
-            parser.parse("src/main/resources/task4/necklace.xml", necklaceParser);
-            System.out.println(necklaceParser.getNecklace());
-        } catch (ParserConfigurationException e) {
-            LOG.error("Parser error", e);
+            validateXml(XSD_PATH, XML_PATH);
         } catch (SAXException e) {
-            LOG.error("Error parsing file", e);
+            LOG.error("Error parsing file!", e);
         } catch (IOException e) {
-            LOG.info("File not found", e);
+            LOG.error("File not found!", e);
         }
 
-        Necklace necklace = new StAXNecklaceParser().parse("src/main/resources/task4/necklace.xml");
-        System.out.println(necklace);
+        Necklace necklaceSAX;
+        try {
+            necklaceSAX = parseUsingSAX(XML_PATH);
+            LOG.info("Parsed by SAX " + necklaceSAX);
+        } catch (ParserConfigurationException e) {
+            LOG.error("Parser error!", e);
+        } catch (SAXException e) {
+            LOG.error("Error parsing file!", e);
+        } catch (IOException e) {
+            LOG.error("File not found!", e);
+        }
+
+        Necklace necklaceStAX;
+        try {
+            necklaceStAX = parseUsingStAX(XML_PATH);
+            LOG.info("Parsed by StAX " + necklaceStAX);
+        } catch (FileNotFoundException e) {
+            LOG.error("File not found!", e);
+        } catch (XMLStreamException e) {
+            LOG.error("Error parsing file!", e);
+        }
+
+        Necklace necklaceDOM;
+        try {
+            necklaceDOM = parseUsingDOM(XML_PATH);
+            LOG.info("Parsed by DOM " + necklaceDOM);
+        } catch (ParserConfigurationException e) {
+            LOG.error("Parser error!", e);
+        } catch (IOException e) {
+            LOG.error("File not found!", e);
+        } catch (SAXException e) {
+            LOG.error("Error parsing file!", e);
+        }
     }
 
-    private static void validateXml(String xsdPath, String xmlPath){
+    private static void validateXml(String xsdPath, String xmlPath) throws SAXException, IOException {
         SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
         File schemaLocation = new File(xsdPath);
         Source xmlFile = new StreamSource(xmlPath);
-        try {
-            Schema schema = factory.newSchema(schemaLocation);
-            Validator validator = schema.newValidator();
-            validator.validate(xmlFile);
-        } catch (SAXException e) {
-            LOG.error(xmlFile.getSystemId() + " is not valid!", e);
-        } catch (IOException e) {
-            LOG.error("File not found!",e);
-        }
+
+        Schema schema = factory.newSchema(schemaLocation);
+        Validator validator = schema.newValidator();
+        validator.validate(xmlFile);
+    }
+
+    private static Necklace parseUsingSAX(String xmlPath) throws ParserConfigurationException, SAXException, IOException {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        javax.xml.parsers.SAXParser parser = factory.newSAXParser();
+        SAXNecklaceParser necklaceParser = new SAXNecklaceParser();
+        parser.parse(xmlPath, necklaceParser);
+        return necklaceParser.getNecklace();
+    }
+
+    private static Necklace parseUsingStAX(String xmlPath) throws FileNotFoundException, XMLStreamException {
+        return new StAXNecklaceParser().parse(xmlPath);
+    }
+
+    private static Necklace parseUsingDOM(String xmlPath) throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(xmlPath);
+        return new DOMParser().parse(document);
     }
 }
